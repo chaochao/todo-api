65523090bc20e6628bb034d4858e4f13efa52a94
Chao Liu <liuchao8989@gmail.com>
2016-10-15 22:35:38 -0700

    add post method

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000
 
-var todos =[
-  {
-    id: 1,
-    description: 'todolist 1',
-    completed: false
-  },
-  {
-    id: 2,
-    description: 'todolist 2',
-    completed: false
-  }
-]
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 app.get('/', function (req,res) {
   res.send('Todo API Root');
 });
@@ -36,6 +28,18 @@ app.get('/todos/:id', function (req,res) {
   } else{
     res.status(404).send();  
   }
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+  var body = req.body;
+  console.log('description: '+ body.description);
+  body.id = todoNextId;
+  todos.push(body);
+    todoNextId++;
+  
+
+  res.json(body);
 });
 
 
