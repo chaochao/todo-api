5fe2a883c89429b48520e96bffe74addc3d94f68
Chao Liu <liuchao8989@gmail.com>
2016-10-15 21:55:31 -0700

    add todos todo/:id route

--- server.js
+++ server.js
@@ -2,8 +2,40 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000
 
+var todos =[
+  {
+    id: 1,
+    description: 'todolist 1',
+    completed: false
+  },
+  {
+    id: 2,
+    description: 'todolist 2',
+    completed: false
+  }
+]
+
 app.get('/', function (req,res) {
   res.send('Todo API Root');
+});
+
+app.get('/todos', function (req,res) {
+  res.json(todos);
+});
+
+app.get('/todos/:id', function (req,res) {
+  var id = parseInt(req.params.id, 10);
+  var matchedTodo;
+  todos.forEach(function (todo) {
+    if (todo.id === id) {
+      matchedTodo = todo;
+    }
+  })
+  if (matchedTodo) {
+    res.json(matchedTodo)
+  } else{
+    res.status(404).send();  
+  }
 });
 
 
