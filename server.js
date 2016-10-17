777ded1fa7e000c320380857f643c6575203c4a2
Chao Liu <liuchao8989@gmail.com>
2016-10-16 17:14:54 -0700

    fix syntax

--- server.js
+++ server.js
@@ -8,104 +8,116 @@ var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
-app.get('/', function (req,res) {
+app.get('/', function(req, res) {
   res.send('Todo API Root');
 });
 // GET ALL
-app.get('/todos', function (req,res) {
+app.get('/todos', function(req, res) {
   var query = req.query;
   var filteredTodos = todos;
 
   var filterAttributes = {};
 
   if (query.hasOwnProperty('completed')) {
-    switch (query.completed){
+    switch (query.completed) {
       case 'true':
-      filterAttributes.completed = true;
-      break;
+        filterAttributes.completed = true;
+        break;
       case 'false':
-      filterAttributes.completed = false;
-      break;
+        filterAttributes.completed = false;
+        break;
     }
   }
   if (query.hasOwnProperty('description')) {
     filterAttributes.description = query.description;
   }
   // use _.where to return
-  filteredTodos = _.where(todos,filterAttributes);
+  filteredTodos = _.where(todos, filterAttributes);
 
   res.json(filteredTodos);
 });
 // GET
-app.get('/todos/:id', function (req,res) {
+app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
+  var matchedTodo = _.findWhere(todos, {
+    id: todoId
+  });
   if (matchedTodo) {
     res.json(matchedTodo)
-  } else{
-    res.status(404).send();  
+  } else {
+    res.status(404).send();
   }
 });
 
 // POST
-app.post('/todos', function (req, res) {
+app.post('/todos', function(req, res) {
   var body = req.body;
-  
-  if (!_.isBoolean(body.completed) || 
-      !_.isString(body.description) ||
-      body.description.trim().length === 0) {
+
+  if (!_.isBoolean(body.completed) ||
+    !_.isString(body.description) ||
+    body.description.trim().length === 0) {
     return res.status(400).send();
 
   }
-  console.log('description: '+ body.description);
-  var body = _.pick(body,'description', 'completed');
+  console.log('description: ' + body.description);
+  var body = _.pick(body, 'description', 'completed');
   body.id = todoNextId++;
   todos.push(body);
   res.json(body);
 });
 
 //PUT
-app.put('/todos/:id', function (req,res) {
+app.put('/todos/:id', function(req, res) {
 
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
+  var matchedTodo = _.findWhere(todos, {
+    id: todoId
+  });
   if (!matchedTodo) {
     return res.status(400).send(`no item with id: ${todoId}`);
   }
-  var body = _.pick(req.body,'description', 'completed');
+  var body = _.pick(req.body, 'description', 'completed');
   var validAttributes = {};
   if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
     validAttributes.completed = body.completed;
-  } else if(body.hasOwnProperty('completed')) {
-    return res.status(400).json({"error":"completed attributes wrong"})
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(400).json({
+      "error": "completed attributes wrong"
+    })
   }
   if (body.hasOwnProperty('description') &&
-      _.isString(body.description) &&
-      body.description.trim().length > 0) {
+    _.isString(body.description) &&
+    body.description.trim().length > 0) {
     validAttributes.description = body.description;
-  } else if(body.hasOwnProperty('description')) {
-    return res.status(400).json({"error":"description attributes wrong"})
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).json({
+      "error": "description attributes wrong"
+    })
   }
   // pass by reference
-   _.extend(matchedTodo,validAttributes);
+  _.extend(matchedTodo, validAttributes);
 
   return res.json(matchedTodo)
 
 });
 
 //DELETE
-app.delete('/todos/:id', function (req,res) {
+app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
+  var matchedTodo = _.findWhere(todos, {
+    id: todoId
+  });
 
   if (matchedTodo) {
-    todos = _.without(todos,matchedTodo);
+    todos = _.without(todos, matchedTodo);
     res.json(matchedTodo);
-  } else{
-    res.status(404).json({"error": `delete item id: ${todoId} fail`});
+  } else {
+    res.status(404).json({
+      "error": `delete item id: ${todoId} fail`
+    });
   }
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log("start express at: " + PORT);
 })
